26157c7822380ebecc37219b1967981ca36d77ed
olusprogr <olus.main333@gmail.com>
2025-11-10 20:36:03 +0100

    adad

--- server.js
+++ server.js
@@ -1,35 +1,44 @@
+const express = require('express');
+const expressListEndpoints = require('express-list-endpoints');
+require('dotenv').config();
+const dbService = require('./db-service');
 const userRoutes = require('./routes/test');
 const emptyRoutes = require('./routes/empty');
 const sshTunnelRoutes = require('./routes/ssh-tunnels');
 const registerUser = require('./routes/register-user');
 const validateUser = require('./routes/validate-user');
-const expressListEndpoints = require('express-list-endpoints');
-const express = require('express');
-require('dotenv').config();
-const dbService = require('./db-service');
 
 const app = express();
-dbService();
+app.use(express.json());
 
-// Configuration
 const PORT = process.env.PORT || 3004;
 const HOST = process.env.HOST || '0.0.0.0';
 const API_PREFIX = process.env.API_PREFIX;
 
-// Endpoints
-app.use(express.json());
-app.use('/', emptyRoutes);
-app.use(API_PREFIX, userRoutes);
-app.use(API_PREFIX, sshTunnelRoutes);
-app.use(API_PREFIX, registerUser);
-app.use(API_PREFIX, validateUser);
+async function startServer() {
+  try {
+    const db = await dbService();
+    app.locals.db = db;
 
-// List Registered Endpoints
-const endpoints = expressListEndpoints(app);
-console.log('Registered Endpoints:', endpoints);
+    // Routen
+    app.use('/', emptyRoutes);
+    app.use(API_PREFIX, userRoutes);
+    app.use(API_PREFIX, sshTunnelRoutes);
+    app.use(API_PREFIX, registerUser);
+    app.use(API_PREFIX, validateUser);
 
-// Start Server
+    // Liste der Endpoints
+    console.log('Registered Endpoints:', expressListEndpoints(app));
 
-app.listen(PORT, HOST, () => {
-  console.log(`Server is running on http://localhost:${PORT}${API_PREFIX}`);
-});
+    // Server starten
+    app.listen(PORT, HOST, () => {
+      console.log(`Server is running on http://${HOST}:${PORT}${API_PREFIX}`);
+    });
+
+  } catch (err) {
+    console.error("Failed to start server:", err);
+    process.exit(1); // optional: Beende Server bei DB-Fehler
+  }
+}
+
+startServer();
