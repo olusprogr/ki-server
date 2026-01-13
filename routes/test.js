1380c5e2702999dadca6be38d3f5e161fb649f2b
olusprogr <olus.main333@gmail.com>
2026-01-13 22:07:16 +0100

    Remove all existing files

--- routes/test.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-router.get('/test', (req, res) => {
-  res.json({ message: 'This is a test route' });
-});
-
-module.exports = router;
